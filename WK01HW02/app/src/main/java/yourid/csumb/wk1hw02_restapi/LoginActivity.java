5bce710c29bb8eab54ed11945b19d260fdd7940c
Xenvex <matthewbreyes@gmail.com>
2021-02-05 19:37:15 -0800

    LoginComplete

--- WK01HW02/app/src/main/java/yourid/csumb/wk1hw02_restapi/LoginActivity.java
+++ WK01HW02/app/src/main/java/yourid/csumb/wk1hw02_restapi/LoginActivity.java
@@ -1,9 +1,12 @@
 package yourid.csumb.wk1hw02_restapi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View v){
+               Log.i("Log Message", "Login Button Clicked");
                //Input Username and Password
                EditText username = findViewById(R.id.username);
                EditText password = findViewById(R.id.password);
@@ -27,6 +31,37 @@ public class LoginActivity extends AppCompatActivity {
                String pw = password.getText().toString();
 
                //Verifying Credentials
+               //Username: din_djarin  Password: baby_yoda_ftw
+               if(name.equals("din_djarin") && pw.equals("baby_yoda_ftw"))
+               {
+                   //Successful Login
+                   //Transition to Main Activity
+                   Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                   startActivity(intent);
+
+               }
+               else
+               {
+                   //Unsuccessful Login
+                   //Display an Error Message
+                   if(!name.equals("din_djarin") && !pw.equals("baby_yoda_ftw")){
+                       //Username and Password are Wrong
+                       TextView msg = findViewById(R.id.message);
+                       msg.setText("Incorrect Username and Password");
+                   }
+                   else if(!pw.equals("baby_yoda_ftw")){
+                       //Just Password is Wrong
+                       TextView msg = findViewById(R.id.message);
+                       msg.setText("Incorrect Password");
+                   }
+                   else if(!name.equals("din_djarin")){
+                       //Just Username is Wrong
+                       TextView msg = findViewById(R.id.message);
+                       msg.setText("Incorrect Username");
+                   }
+
+
+               }
 
            }
         });
