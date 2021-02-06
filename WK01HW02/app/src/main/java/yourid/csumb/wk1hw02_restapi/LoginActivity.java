c485c536f75859e4adae3b406ef54c0d22b9556a
Xenvex <matthewbreyes@gmail.com>
2021-02-05 18:35:07 -0800

    LoginUpdated

--- WK01HW02/app/src/main/java/yourid/csumb/wk1hw02_restapi/LoginActivity.java
+++ WK01HW02/app/src/main/java/yourid/csumb/wk1hw02_restapi/LoginActivity.java
@@ -14,11 +14,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button login_button = findViewById(R.id.login_button);
+        //Verifying Credentials on ButtonClick
         login_button.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View v){
+               //Input Username and Password
                EditText username = findViewById(R.id.username);
                EditText password = findViewById(R.id.password);
+
+               //Converting the values into Strings
+               String name = username.getText().toString();
+               String pw = password.getText().toString();
+
+               //Verifying Credentials
 
            }
         });
